20391dd19405b4f717aa73263b6e6ef2a440371a
MARIO CAMPOS MOCHOLI <m.campos@cleverpy.com>
2021-12-18 12:36:31 +0100

    Update

--- bank-solution.py
+++ bank-solution.py
@@ -1,15 +1,53 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import ConfusionMatrixDisplay, recall_score
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler, Normalizer, LabelEncoder
 from sklearn.svm import SVC
+from sklearn.cluster import KMeans
+from sklearn.utils import multiclass
+
+
+def base_evaluation(models, scalers, X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, stratify=y)
+    for model in models:
+        for scaler in scalers:
+            print("Model: ", model, "\tScaler: ", scaler)
+            if scaler is not None:
+                X_train_scaled = scaler.fit_transform(X_train)
+                X_test_scaled = scaler.transform(X_test)
+            else:
+                X_train_scaled = X_train
+                X_test_scaled = X_test
+            model.fit(X_train_scaled, y_train)
+            y_pred = model.predict(X_test_scaled)
+            print('Acuracy: ', model.score(X_test_scaled, y_test))
+            print('Recall: ', recall_score(y_test, y_pred))
+            # ConfusionMatrixDisplay.from_estimator(model, X_test_scaled, y_test)
+            # plt.show()
+
+
+def cross_evaluation(models, scalers, X, y):
+    for model in models:
+        for scaler in scalers:
+            print("Model: ", model, "\tScaler: ", scaler)
+            if scaler is not None:
+                X_scaled = scaler.fit_transform(X)
+            else:
+                X_scaled = X
+            scores = cross_val_score(
+                model, X_scaled, y, cv=5, scoring='recall')
+            print('Recall: ', scores.mean())
+
 
 if __name__ == '__main__':
     df = pd.read_csv('bank/bank-full.csv', sep=';')
 
-    # Data Preprocessing
+    # Common Data Preprocessing
     for column in df.columns:
         if df[column].dtype == type(object):
             le = LabelEncoder()
@@ -18,21 +56,23 @@ if __name__ == '__main__':
     X = df.drop('y', axis=1)
     y = df['y']
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2)
-
-    # Model Training
+    # Models
+    lr = LogisticRegression(multi_class='ovr', solver='liblinear')
+    k_means = KMeans()
     knn = KNeighborsClassifier(n_neighbors=5)
-    knn.fit(X_train, y_train)
-    print('KNN train score: ', knn.score(X_train, y_train))
-    print('KNN Accuracy: ', knn.score(X_test, y_test))
-
-    rf = RandomForestClassifier(n_estimators=100)
-    rf.fit(X_train, y_train)
-    print('RF train score: ', rf.score(X_train, y_train))
-    print('RF Accuracy: ', rf.score(X_test, y_test))
-
+    rf = RandomForestClassifier(n_estimators=200)
     svm = SVC()
-    svm.fit(X_train, y_train)
-    print('SVM train score: ', svm.score(X_train, y_train))
-    print('SVM Accuracy: ', svm.score(X_test, y_test))
+
+    models = [lr, k_means, knn, rf, svm]
+
+    # Preprocessing
+    scaler = StandardScaler()
+    scaler2 = MinMaxScaler()
+    scaler3 = MaxAbsScaler()
+    scaler4 = RobustScaler()
+    scaler5 = Normalizer()
+
+    scalers = [None, scaler, scaler2, scaler3, scaler4, scaler5]
+
+    # base_evaluation(models, X, y)
+    cross_evaluation(models, scalers, X, y)
